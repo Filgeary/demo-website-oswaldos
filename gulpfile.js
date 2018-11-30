6fbfaa6da5f5cbcb9b9df11c0946a063b8a52774
Georgiy Filippov <g.filippov777@gmail.com>
2018-11-30 23:49:53 +0500

    add gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('style', function () {
   return gulp.src("src/sass/style.scss")
+    .pipe(plumber())
     .pipe(sass())
+    .pipe(plumber.stop())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
