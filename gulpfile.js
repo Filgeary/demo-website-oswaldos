0897cd414ca17611999b4766f1f8eaa7549fbd20
Georgiy Filippov <g.filippov777@gmail.com>
2018-11-30 23:15:28 +0500

    add local server

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,28 @@
-'use strict';
-
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-
-sass.compiler = require('node-sass');
+var browserSync = require('browser-sync').create();
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'))
+gulp.task('style', function () {
+  return gulp.src("src/sass/style.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function () {
+gulp.task('serve', ['style'], function () {
 
   browserSync.init({
-    server: ".",
+    server: "./src",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("src/sass/*.scss", ['sass']);
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("src/sass/*.scss", ['style']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
