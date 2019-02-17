222ffed4d7ffa1db0d7012c886e95ff62ca9126d
Georgiy Filippov <g.filippov777@gmail.com>
2019-02-17 21:47:39 +0500

    add 'gulp-postcss, autoprefixer, gulp-csso, gulp-rename' plugins

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,25 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
-// Compile SASS into CSS & auto-inject into browsers
+// Compile SASS into CSS, add Autoprefixer, Minify, Rename & auto-inject into browsers
 gulp.task('style', function () {
   return gulp.src("src/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(plumber.stop())
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(browserSync.stream());
 });
 
@@ -28,7 +38,7 @@ gulp.task('serve', ['style'], function () {
     ui: false
   });
 
-// Watch files
+  // Watch files
   gulp.watch("src/sass/**/*.scss", ['style']);
   gulp.watch("src/*.html").on('change', browserSync.reload);
   gulp.watch("src/js/*.js").on('change', browserSync.reload);
