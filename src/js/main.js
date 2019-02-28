c9de042ae2d3bdda47376a6c88138d21cae1acf4
Georgiy Filippov <g.filippov777@gmail.com>
2019-02-28 21:59:02 +0500

    add script for 'modal__login'

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,54 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// Popup User-Login
+var loginLink = document.querySelector('.user-list__login');
+
+var popupUserLogin = document.querySelector('.modal__login');
+var popupClose = popupUserLogin.querySelector('.modal__close-btn');
+
+var login = popupUserLogin.querySelector('[name=login]');
+var password = popupUserLogin.querySelector('[name=password]');
+
+// Write Data to Local Storage
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+// Show Modal on click 'loginLink'
+loginLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupUserLogin.classList.add('modal--show');
+
+  // Focus on form input
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+});
+
+// Close Modal on click 'popupClose'
+popupClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupUserLogin.classList.remove('modal--show');
+});
+
+// Close Modal on click 'ESC'
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupUserLogin.classList.contains('modal--show')) {
+      popupUserLogin.classList.remove('modal--show');
+    }
+  }
+});
+
+// mockup
